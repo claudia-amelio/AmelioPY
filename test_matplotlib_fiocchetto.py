3c2eab6072a6a6f44d07c47e7390619da490a880
claudietti <claudia.amelio.s13@liceoviconapoli.it>
2021-03-01 17:05:00 +0100

    Update test_matplotlib_fiocchetto.py

--- test_matplotlib_fiocchetto.py
+++ test_matplotlib_fiocchetto.py
@@ -1,6 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
+<<<<<<< Updated upstream
 x = np.array([5,13,21,10,10,22,24,23,11,17,33,99])
 y = np.array([99,33,17,11,23,24,22,10,10,21,13,5])
+=======
+
+
+x = np.array([5,11,13,21,24,25,71,77,99])
+y = np.array([99,77,71,25,24,21,13,11,5])
+
+x.sort()
+>>>>>>> Stashed changes
 plt.plot(x,y)
 plt.show()
