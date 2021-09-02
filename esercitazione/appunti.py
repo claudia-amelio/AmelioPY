9171d780851916223bb7aab5f6c39a85d3709104
Claudia <74513728+claudia-amelio@users.noreply.github.com>
2021-09-03 00:05:11 +0200

    Create appunti.py
    esercitazione 03/09

---
+++ esercitazione/appunti.py
@@ -0,0 +1,9 @@
+input(list('Mario, Giovanni, Paola'))
+input(list('matematica, italiano, scienze, inglese')
+Mario = {
+    matematica = 6
+    italiano = 8
+    scienze = 7
+    inglese = 10
+    }
+    
