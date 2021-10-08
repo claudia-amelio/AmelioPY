7c711a9085a6f2d30e2427d9bf409f8a8d97d118
Claudia <74513728+claudia-amelio@users.noreply.github.com>
2021-10-08 20:12:28 +0200

    aggiornamenti

--- esercitazione/oop/calcComb_amelio.py
+++ esercitazione/oop/calcComb_amelio.py
@@ -21,11 +21,27 @@ class calcComb:
     def permutazioni(self, k):      #metodo che fa pare della calcomb
         return len(self.__stringa)**k #k è il valore dei posti della combinazione
 
-    def posizioni(self,k):
-        return 
+    def disposizioni(self,k,):
+        while k>0:
+            y  = len(self.__stringa)*k
+            k=-1
+            c=+y
+        return c
+
+
+    
+    #def eliminazioniRipetizioni(self):
+        #for lettera in GalileoGalilei.getStringalist:
+          #  str.count(lettera)
+          #  i=+lettera
 #impostando le variabili, che diventano degli oggetto
 
-GalileoGalilei = calcComb("Misura ciò che è misurabile, e rendi misurabile ciò che non lo è.") #è un oggetto che si trova in questa classe di oggetti)
-print(GalileoGalilei.permutazioni(2)) 
-print(GalileoGalilei.stringa)
-print(GalileoGalilei.getStringa())
+GalileoGalilei = calcComb("stringaDiRiferimento") #è un oggetto che si trova in questa classe di oggetti)
+
+GalileoGalilei.setStringa("da")
+print(GalileoGalilei.permutazioni(5)) 
+#print(GalileoGalilei.eliminazioniRipetizioni())
+print (GalileoGalilei.disposizioni(5))
+
+
+
