8b0276bf4fa30d4680e578030fa7df2471222ecd
Claudia <74513728+claudia-amelio@users.noreply.github.com>
2021-10-16 23:21:11 +0200

    creazione dell'oggetto

--- oop/calcComb_amelio.py
+++ oop/calcComb_amelio.py
@@ -1,5 +1,6 @@
 
 #IMPOSTANDO IL PROGRAMMA
+
 def factorial(n):
     fact = 1
     for num in range(2, n + 1):
@@ -49,3 +50,17 @@ class calcComb: #costruzione classe
                 n=len(self.__stringa)
                 return factorial(n)
 
+    #def lettereMaiuscole(self):
+     #   self.__stringa = upper(str)
+      #  self.stringalist = list(upper(str))
+
+#CREAZIONE DELL'OGGETTO
+
+galileoGalilei = calcComb("stringaDiRiferimento") #è un oggetto che si trova in questa classe di oggetti)
+galileoGalilei.setStringa("astronomo")
+
+print("la lista relativa alla stringa è:",galileoGalilei.getStringalist())
+print("la stringa è",galileoGalilei.getStringa())
+print("il numero di disposizioni con ripetizioni è:",galileoGalilei.disposizioniConRipetizioni(5)) 
+print ("il numero di disposizioni semplici è:",galileoGalilei.disposizioniSemplici(2))
+print ("il risultato della permutazione è:",galileoGalilei.permutazioniSemplici())
